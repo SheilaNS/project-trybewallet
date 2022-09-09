fa475dd8297895d0bd7c02c3b9b79ceb2e63e3ec
SheilaNS <shei.nsantos@gmail.com>
2022-09-09 18:31:23 -0300

    fix(Fixing the index.js): :bug:

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import store from './store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={ store }>
+  <Provider store={ store }>
+    <BrowserRouter>
       <App />
-    </Provider>
-  </BrowserRouter>,
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
 
