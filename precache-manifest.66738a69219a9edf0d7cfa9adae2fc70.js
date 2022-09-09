b52331edb88724de9cae8b374984c0037b3733a5
SheilaNS <shei.nsantos@gmail.com>
2022-09-09 15:51:50 -0300

    Updates

---
+++ precache-manifest.66738a69219a9edf0d7cfa9adae2fc70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc60bb6026ef84ee3e408b8647986f25",
+    "url": "/project-trybewallet/index.html"
+  },
+  {
+    "revision": "97cd62e20dcba128c543",
+    "url": "/project-trybewallet/static/css/main.194859b8.chunk.css"
+  },
+  {
+    "revision": "2478865d304d20d2e948",
+    "url": "/project-trybewallet/static/js/2.4d85d8cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-trybewallet/static/js/2.4d85d8cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97cd62e20dcba128c543",
+    "url": "/project-trybewallet/static/js/main.cb87d1e2.chunk.js"
+  },
+  {
+    "revision": "f7e9fac6e1746606b827",
+    "url": "/project-trybewallet/static/js/runtime-main.29301b93.js"
+  }
+]);
